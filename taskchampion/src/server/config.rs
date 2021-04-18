a8237efc97930070a2b03727ff24a86b0317be7d
Dustin J. Mitchell <dustin@mozilla.com>
2021-04-18 23:40:57 +0000

    remove remote server implementation

--- taskchampion/src/server/config.rs
+++ taskchampion/src/server/config.rs
@@ -1,7 +1,6 @@
 use super::types::Server;
-use super::{LocalServer, RemoteServer};
+use super::LocalServer;
 use std::path::PathBuf;
-use uuid::Uuid;
 
 /// The configuration for a replica's access to a sync server.
 pub enum ServerConfig {
@@ -10,18 +9,6 @@ pub enum ServerConfig {
         /// Path containing the server's DB
         server_dir: PathBuf,
     },
-    /// A remote taskchampion-sync-server instance
-    Remote {
-        /// Sync server "origin"; a URL with schema and hostname but no path or trailing `/`
-        origin: String,
-
-        /// Client Key to identify and authenticate this replica to the server
-        client_key: Uuid,
-
-        /// Private encryption secret used to encrypt all data sent to the server.  This can
-        /// be any suitably un-guessable string of bytes.
-        encryption_secret: Vec<u8>,
-    },
 }
 
 impl ServerConfig {
@@ -29,11 +16,6 @@ impl ServerConfig {
     pub fn into_server(self) -> anyhow::Result<Box<dyn Server>> {
         Ok(match self {
             ServerConfig::Local { server_dir } => Box::new(LocalServer::new(server_dir)?),
-            ServerConfig::Remote {
-                origin,
-                client_key,
-                encryption_secret,
-            } => Box::new(RemoteServer::new(origin, client_key, encryption_secret)),
         })
     }
 }
--- taskchampion/src/server/mod.rs
+++ taskchampion/src/server/mod.rs
@@ -13,10 +13,8 @@ pub(crate) mod test;
 
 mod config;
 mod local;
-mod remote;
 mod types;
 
 pub use config::ServerConfig;
 pub use local::LocalServer;
-pub use remote::RemoteServer;
 pub use types::*;
